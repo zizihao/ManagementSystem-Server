b0c410e925916f7ff3be42c8eae9040831320871
chenzihao <chenzihao_2020@163.com>
2020-12-08 21:02:18 +0800

    增加jwt

--- routes/login.js
+++ routes/login.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const { retrieveUsername } = require('../model/main')
-
+const jwt = require('../util/jwt')
 
 /* GET users listing. */
 router.post('/', function (req, res, next) {
-    console.log(req.body);
-    retrieveUsername(req.body.username, (err, data) => {
+    const {username,password} = req.body
+    retrieveUsername(username, (err, data) => {
         if (!err) {
             if (data.length === 0) {
                 res.send({ code: 201, messge: "登录失败，用户名不存在" });
-            } else if (req.body.password === data[0].password) {
-                res.send({ code: 200, messge: "登录成功" });
+            } else if (password === data[0].password) {
+                res.send({ 
+                    code: 200, 
+                    messge: "登录成功" ,
+                    token:jwt.createToken(username)
+                });
             }
             else {
                 res.send({ code: 201, messge: "登录失败，账号或密码错误" });
